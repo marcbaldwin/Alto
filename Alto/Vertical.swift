8e5599154cbe351c04327e910ae68f732c14194f
Marc Baldwin <marc.baldwin88@gmail.com>
2018-04-16 13:08:37 +0100

    Add vertical layout support for multiple views

--- Alto/Vertical.swift
+++ Alto/Vertical.swift
@@ -43,11 +43,21 @@ public extension Array where Element: UIView {
 
     @discardableResult
     public func set(_ attr1: Vertical, _ relation: Relation, _ view: UIView, _ attr2: Vertical, priority: Priority? = nil, isActive: Bool = true) -> [NSLayoutConstraint] {
-        return NSLayoutConstraint.create(self, attr1, relation, view, attr2, priority: priority, isActive: isActive)
+        return map { $0.set(attr1, relation, view, attr2, priority: priority, isActive: isActive) }
     }
 
     @discardableResult
     public func set(_ attr1: Vertical, _ relation: Relation, _ view: UIView, _ attr2: MultiplierOffset<Vertical>, priority: Priority? = nil, isActive: Bool = true) -> [NSLayoutConstraint] {
-        return NSLayoutConstraint.create(self, attr1, relation, view, attr2, priority: priority, isActive: isActive)
+        return map { $0.set(attr1, relation, view, attr2, priority: priority, isActive: isActive) }
+    }
+
+    @discardableResult
+    public func set(_ attr1: Vertical, _ relation: Relation, _ view: UILayoutSupport, _ attr2: Vertical, priority: Priority? = nil, isActive: Bool = true) -> [NSLayoutConstraint] {
+        return map { $0.set(attr1, relation, view, attr2, priority: priority, isActive: isActive) }
+    }
+
+    @discardableResult
+    public func set(_ attr1: Vertical, _ relation: Relation, _ view: UILayoutSupport, _ attr2: MultiplierOffset<Vertical>, priority: Priority? = nil, isActive: Bool = true) -> [NSLayoutConstraint] {
+        return map { $0.set(attr1, relation, view, attr2, priority: priority, isActive: isActive) }
     }
 }
