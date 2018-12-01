22347c9894c5be7c725064bda7cbb8ff6aefadb5
Marc Baldwin <marc.baldwin88@gmail.com>
2018-12-01 23:13:01 +0000

    Add ConstraintBasedStackView

---
+++ Alto/ConstraintBasedStackView.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+@available(iOS 9.0, *)
+open class StackView: UIStackView {
+
+    open var hasInitConstraints = false
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        initView()
+    }
+
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)
+        initView()
+    }
+
+    open func initView() {
+
+    }
+
+    override open func updateConstraints() {
+        super.updateConstraints()
+        if !hasInitConstraints {
+            hasInitConstraints = true
+            initConstraints()
+        }
+    }
+
+    open func initConstraints() {
+
+    }
+}
