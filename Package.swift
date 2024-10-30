457999d2dc611335d3625bac2bd56526105f74b4
Marc Baldwin <marc.baldwin88@gmail.com>
2024-10-30 11:10:31 +0000

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+  name: "Alamofire",
+  platforms: [.macOS(.v10_13),
+              .iOS(.v12),
+              .tvOS(.v12),
+              .watchOS(.v4)],
+  products: [
+      .library(name: "Alto", targets: ["Alto"]),
+  ],
+  targets: [.target(name: "Alto",
+                    path: "Alto",
+                    exclude: ["Info.plist"],
+                    swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
+                    ),
+            .testTarget(name: "AltoTests",
+                        dependencies: ["Alto"],
+                        path: "Tests",
+                        exclude: ["Info.plist"],
+                        )
+  ],
+  swiftLanguageModes: [.v5])
